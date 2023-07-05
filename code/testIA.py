6f403d7035c68ea55df95a21e6791edc9aa18fdc
montti@posteo.net <montti>
2023-07-05 13:56:38 -0400

    Dummy model and webserver

---
+++ code/testIA.py
@@ -0,0 +1,16 @@
+from tensorflow import keras
+from dlgo.agent.predict import DeepLearningAgent
+from dlgo.encoders.oneplane import OnePlaneEncoder
+from dlgo import httpfrontend
+from dlgo import mcts
+
+
+encoder = OnePlaneEncoder((9, 9))
+
+model = keras.models.load_model("deep6.h5")
+
+deep = DeepLearningAgent(model, encoder)
+
+web_app = httpfrontend.get_web_app({'deep': deep})
+web_app.run()
+
